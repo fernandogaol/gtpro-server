c3b8e36617e03497581fcd76f4100e0287fb45bf
Fernando Gaol <ffilho@riolindo.org>
2020-03-23 21:44:49 -0600

    added users post method

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const app = express();
+const usersRouter = require('../src/users/users-route');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/users', usersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
