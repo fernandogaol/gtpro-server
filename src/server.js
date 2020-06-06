54686e8d2563a85d562ee02472d0aba0391ea534
Fernando Gaol <ffilho@riolindo.org>
2020-06-06 17:37:59 -0600

    testing heroku push

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
 
+console.log('Testing, testing, hello world!');
+console.log(DATABASE_URL);
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
