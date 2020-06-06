3d2188f970049555a350ab4f6dd9214f6c975e8f
Fernando Gaol <ffilho@riolindo.org>
2020-06-06 16:40:55 -0600

    trying to push to heroku

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ const db = knex({
 
 app.set('db', db);
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(PORT || 4000, function () {
+  console.log('Your node js server is running');
 });
